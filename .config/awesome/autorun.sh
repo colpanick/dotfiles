7b3362f89c1ebed43d8a3ef6e4cdb3ac317eafa5
George Miles <gmiles83@gmail.com>
2020-02-14 23:17:07 -0500

    Added awesome config and theme

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function run {
+    if ! pgrep -f $1 ;
+    then
+        $@&
+    fi
+}
+
+run /usr/local/share/light_dm/startup.sh
+run /usr/bin/VBoxClient-all
+run /usr/bin/imwheel
